05dafff9d7415938fc2d8246cd5dc91df9732612
Shahzaib Nawaz <93723415+Shahzaib012@users.noreply.github.com>
2023-02-21 22:05:56 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,119 @@
+var name ;
+name = "Shahzaib Nawaz";
+var admin;
+
+admin = name;
+alert(admin);
+
+// // Question No : 2
+
+var earth;
+earth = prompt("Name Of Your Planet");
+alert(earth);
+
+// // Question No : 3
+
+var a = prompt("Name, The Current Visitor");
+alert(a);
+
+// // Question no : 4
+
+var favDrink;
+favDrink =  "Your Favorite Drink , Sting";
+// console.log(favDrink);
+alert(favDrink);
+
+// // Question no : 5
+
+var favNum;
+favNum = 50;
+alert(favNum);
+console.log(favNum);
+
+// Question No : 6
+
+function myFavs(){
+    console.log("hhhhh");
+}
+// invoking
+myFavs();
+
+// // Question No 7
+
+let person = {
+    name :"Shahzaib",
+    age : 20,
+}
+console.log(person);
+
+// Question No : 8
+
+var a = 26;
+var b =  24;
+c = a+b;
+console.log(c);
+
+// In the Form of functions
+
+function sum(num1,num2){
+    return num1 + num2
+
+}
+var a = sum(5,9);
+console.log(a);
+
+
+// Question No : 9
+
+var a = 1080;
+var b = 36;
+console.log(a/b);
+console.log("Result of firstProgram = ",a);
+
+// Question No : 10
+
+
+var a = 50;
+var b = 50;
+c = a+b;
+d = c/5;
+console.log("Answer by Dividing two numbers After thheir's Sum =" ,d);
+console.log("Result of the firstProgram is = ", a);
+
+// Question No : 11
+  var tempF = 50;
+  f = tempF;
+  var tempC = ((f-32)*5/9);
+  console.log(tempC);
+
+// Question No : 12
+
+var a = parseInt (prompt("First Number"));
+var b = parseInt (prompt("Second Number"));
+alert(a+b);
+
+// Question No 13
+
+var a = prompt();
+var b = prompt();
+console.log(a-b);
+
+// // Question No 14
+
+var a = prompt();
+var b = prompt();
+alert(a*b);
+
+// Question No 15
+
+var a = prompt();
+var b = prompt();
+quotient = a/b;
+console.log("Quotient",quotient);
+
+// Question No 16
+
+var a = prompt();
+var b = prompt();
+remainder = a%b;
+alert(remainder);
